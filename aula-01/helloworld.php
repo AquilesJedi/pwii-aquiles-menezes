dee0b676f9a3cc4bef4f4774f1ffa5f93d38c306
Aquiles Jedi <aquiles.jedi@gmail.com>
2025-02-25 16:32:22 -0300

    Aula de PHP com exemplos de concatenação de variaveis

---
+++ aula-01/helloworld.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <body>
+ 
+        <h1>Teste de PHP</h1>
+
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK2EGu60f59DEwfINdNJPnThKBykuok2uZ4w&s" alt="">
+        
+        <?php
+        // slk
+        echo "Hello World! <br>";
+        $color = "red";
+        echo "My car is " . $color . "<br>";
+
+        $x = "tyler";
+        $y = "mugshot";
+        $nome = "Aquiles";
+        $sobrenome = "Menezes";
+
+        echo $x, "<br>";
+        echo $y, "<br>";
+        echo "${nome} <h1>${sobrenome}</h1>"
+        ?>
+
+    </body>
+</html>
