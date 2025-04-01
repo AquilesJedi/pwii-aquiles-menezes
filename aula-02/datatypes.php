ce5f1ee694d2b1cb39c3bb01b3854bc2c97905b8
Aquiles Jedi <aquiles.jedi@gmail.com>
2025-04-01 16:25:02 -0300

    Arquivo novo: DataTypes

---
+++ aula-02/datatypes.php
@@ -0,0 +1,23 @@
+<?php
+$x = "Hello World!";
+$y = "Kon'nichiwa sekai!";
+
+var_dump($x);
+echo "<br>";
+var_dump($y);
+echo "<br>";
+
+$x = 12.3;
+var_dump($x);
+echo "<br><br>";
+
+$y = ['João','Ricardo', 2];
+var_dump($y);
+echo "<br>";
+echo $y[1];
+echo "<br>";
+
+$meuObj -> nome : "Davi";
+$meuObj -> idade : 42;
+ 
+?>
