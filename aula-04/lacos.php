8b559367883276037bba58923641073ba20c1afb
Aquiles Jedi <aquiles.jedi@gmail.com>
2025-04-29 16:24:38 -0300

    Laços de Repetições

---
+++ aula-04/lacos.php
@@ -0,0 +1,29 @@
+<?php
+
+    //WHILE
+    echo "While <br>";
+    $contador = 1;
+
+    while ($contador < 6) {
+        echo $contador, "<br>";
+        $contador++;
+    }
+
+    //DO WHILE  
+    echo "Do While <br>";
+
+    $contadorii = 1;
+
+    do {
+        echo $contadorii , "<br>";
+        $contadorii++;
+    } while ($contadorii < 6);
+
+    //FOR
+    echo "<br> For <br>";
+
+    for ($contadoriii = 0; $contadoriii <= 10; $contadoriii++) {
+        echo $contadoriii, "<br>";
+    }
+
+?>
