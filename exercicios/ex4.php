22bf60414319d13cc5b1df339bddecb04c11444d
AquilesJedi <135276614+AquilesJedi@users.noreply.github.com>
2025-05-29 13:52:24 -0300

    Update ex4.php

--- exercicios/ex4.php
+++ exercicios/ex4.php
@@ -1,6 +1,6 @@
 <html>
     <body>
-        <form action="ex4-pt.php" method="post">
+        <form action="ex4-pt.php" method="get">
             <label for="preço">qual o preço do produto?</label>
             <input type="number" name="preço" id="preço"> <br>
             <input type="submit" value="Resultado">
