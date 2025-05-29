04544f9f30f1692cab72ab78592de54097905e59
AquilesJedi <135276614+AquilesJedi@users.noreply.github.com>
2025-05-29 13:51:51 -0300

    Update ex2.php

--- exercicios/ex2.php
+++ exercicios/ex2.php
@@ -1,6 +1,6 @@
 <html>
     <body>
-    <form action="ex2.pt2.php" method="post">
+    <form action="ex2.pt2.php" method="get">
         <label for="comprimento">Qual o comprimento da caixa em cm?</label>
         <input type="number" name="comprimento" id="comprimento"> <br>
         <label for="largura">Qual a largura da caixa em cm?</label>
