4f01703b9d16e272ce2a74f539f91c836df39edd
AquilesJedi <135276614+AquilesJedi@users.noreply.github.com>
2025-05-29 13:51:39 -0300

    Update ex1.php

--- exercicios/ex1.php
+++ exercicios/ex1.php
@@ -1,6 +1,6 @@
 <html>
 <body>
-    <form action="ex1-pt2.php" method="post">
+    <form action="ex1-pt2.php" method="get">
         <label for="dist">Qual a Distância em KM?</label> 
         <input type="number" name="dist" id="dist"><br>
         <label for="gas">Qual a quantidade gasta de Gasolina em litros?</label> 
